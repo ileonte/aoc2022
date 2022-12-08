3b7c4ad0572002e62dadd0e3d4e80b1f6560b96c
Ionuț Leonte <ionut.leonte@gmail.com>
2022-12-08 12:58:43 +0200

    full run script

---
+++ zig/run.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+BASE=$(dirname "$0")
+
+runTests() {
+    ${BASE}/zig-out/bin/day01 < ${BASE}/../inputs/day01.txt
+    ${BASE}/zig-out/bin/day02 < ${BASE}/../inputs/day02.txt
+    ${BASE}/zig-out/bin/day03 < ${BASE}/../inputs/day03.txt
+    ${BASE}/zig-out/bin/day04 < ${BASE}/../inputs/day04.txt
+    ${BASE}/zig-out/bin/day05 < ${BASE}/../inputs/day05.txt
+    ${BASE}/zig-out/bin/day06 < ${BASE}/../inputs/day06.txt
+    ${BASE}/zig-out/bin/day07 < ${BASE}/../inputs/day07.txt
+    ${BASE}/zig-out/bin/day08 < ${BASE}/../inputs/day08.txt
+}
+
+time runTests
