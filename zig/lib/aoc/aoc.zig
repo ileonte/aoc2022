da5f8d54b9ade5ad832e78fe9fff6482724cc599
Ionuț Leonte <ionut.leonte@gmail.com>
2022-12-08 12:01:09 +0200

    tweaks

--- zig/lib/aoc/aoc.zig
+++ zig/lib/aoc/aoc.zig
@@ -1,73 +1,63 @@
 const std = @import("std");
 
-pub const Stream = struct {
-    pub const Type = enum {
-        File,
-        Memory,
-    };
-
-    type: Type,
-    file: ?*std.fs.File,
-    mem: ?*std.io.FixedBufferStream([] const u8),
+pub const Stream = union(enum) {
+    file: *std.fs.File,
+    mem: *std.io.FixedBufferStream([] const u8),
 
     const Self = @This();
 
     pub fn initFile(file: *std.fs.File) Self {
         return .{
-            .type = .File,
             .file = file,
-            .mem = null,
         };
     }
 
     pub fn initMem(mem: *std.io.FixedBufferStream([] const u8)) Self {
         return .{
-            .type = .Memory,
-            .file = null,
             .mem = mem,
         };
     }
 
     pub fn getPos(self: *const Self) !u64 {
-        return switch (self.type) {
-            .File   => self.file.?.getPos(),
-            .Memory => self.mem.?.getPos(),
+        return switch (self.*) {
+            Stream.file => |file| file.getPos(),
+            Stream.mem  => |memf| memf.getPos(),
         };
     }
 
     pub fn seekTo(self: *const Self, pos: u64) !void {
-        return switch (self.type) {
-            .File   => self.file.?.seekTo(pos),
-            .Memory => self.mem.?.seekTo(pos),
+        return switch (self.*) {
+            Stream.file => |file| file.seekTo(pos),
+            Stream.mem  => |memf| memf.seekTo(pos),
         };
     }
 
     pub fn read(self: *const Self, buffer: []u8) ![]u8 {
-        var count = switch (self.type) {
-            .File   => try self.file.?.reader().read(buffer),
-            .Memory => try self.mem.?.reader().read(buffer),
+        var count = switch (self.*) {
+            Stream.file => |file| try file.reader().read(buffer),
+            Stream.mem  => |memf| try memf.reader().read(buffer),
         };
         return buffer[0..count];
     }
 
     pub fn readUntilDelimiterOrEof(self: *const Self, buf: []u8, delimiter: u8) !?[]u8 {
-        return switch (self.type) {
-            .File   => self.file.?.reader().readUntilDelimiterOrEof(buf, delimiter),
-            .Memory => self.mem.?.reader().readUntilDelimiterOrEof(buf, delimiter),
+        return switch (self.*) {
+            Stream.file => |file| file.reader().readUntilDelimiterOrEof(buf, delimiter),
+            Stream.mem  => |memf| memf.reader().readUntilDelimiterOrEof(buf, delimiter),
         };
     }
 
     pub fn readUntilDelimiterOrEofAlloc(self: *const Self, allocator: std.mem.Allocator, delimiter: u8, max_size: usize) !?[]u8 {
-        return switch (self.type) {
-            .File   => self.file.?.reader().readUntilDelimiterOrEofAlloc(allocator, delimiter, max_size),
-            .Memory => self.mem.?.reader().readUntilDelimiterOrEofAlloc(allocator, delimiter, max_size),
+        return switch (self.*) {
+            Stream.file => |file| file.reader().readUntilDelimiterOrEofAlloc(allocator, delimiter, max_size),
+            Stream.mem  => |memf| memf.reader().readUntilDelimiterOrEofAlloc(allocator, delimiter, max_size),
         };
     }
 
     pub fn readAll(self: *const Self, buf: []u8) !usize {
-        return switch (self.type) {
-            .File   => self.file.?.readAll(buf),
-            .Memory => self.mem.?.read(buf),
+        return switch (self.*) {
+            Stream.file => |file| file.readAll(buf),
+            Stream.mem  => |memf| memf.read(buf),
         };
     }
 };
